0081e32f38f76f4588cb44f1b39757f70ea5d0ad
anny026 <anny26@list.ru>
2024-02-27 16:53:38 +0300

    Add sout !

--- first-git/src/main/java/org/example/Main.java
+++ first-git/src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("Test!");
         System.out.println("I now");
         System.out.println("I now more");
+        System.out.println("!!!");
     }
 
 }
